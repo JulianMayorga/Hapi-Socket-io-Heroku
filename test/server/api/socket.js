451e881d87eca8403b075df8c9d60d29a92cb291
JulianMayorga <mayorga.julian@gmail.com>
2015-01-25 21:54:15 +0000

    test(socket): test socket:send:name message

---
+++ test/server/api/socket.js
@@ -0,0 +1,58 @@
+var Lab = require('lab');
+var Code = require('code');
+var Hapi = require('hapi');
+var io = require('socket.io-client');
+
+var Config = require('../../../config');
+var Socket = require('../../../server/api/socket');
+
+
+var lab = exports.lab = Lab.script();
+var request, server, client, socketURL, options;
+
+
+lab.beforeEach(function (done) {
+
+    server = new Hapi.Server();
+    server.connection({ port: Config.get('/port/web') });
+
+    socket = Object.create(Socket);
+    socket.initialize(server.listener);
+
+    socketURL = server.info.uri;
+    options ={
+        transports: ['websocket'],
+        forceNew: true
+    };
+    
+    server.start(done);
+});
+
+
+lab.experiment('Socket', function () {
+
+    lab.beforeEach(function (done) {
+
+        client = io.connect(socketURL, options);
+
+        done();
+    });
+    
+    lab.afterEach(function (done) {
+
+        client.disconnect();
+
+        done();
+    });
+
+
+    lab.test('returns socket:send:name message on connection', function (done) {
+
+        client.on('socket:send:name', function (response) {
+
+            Code.expect(response.name).to.equal('Bob');
+
+            done();
+        });
+    });
+});
